ee3fae68cf912c42a3b319100527a45b4f6f55d1
ungitamas <ungi.tamas@moriczref.hu>
2024-04-19 11:03:00 +0200

    pénteki állapot

--- musor.py
+++ musor.py
@@ -38,4 +38,17 @@ with open("musor.csv", "r", encoding="utf-8") as bemenet:
         zene["cim"] = adatok[3].split(":")[1]
         zenek.append(zene)
         zene = {}
-# Itt fejeztük be
+
+print(f"{len(zenek)} db dal szerepel a fáljban.")
+
+statisztika = {}
+for zene in zenek: 
+    if zene['eloado'] in statisztika: 
+        statisztika[zene['eloado']] += 1 
+    else: 
+        statisztika[zene['eloado']] = 1 
+
+with open("musor_statisztika.txt", "w", encoding="utf-8") as kimenet:
+    for kulcs in statisztika:
+        print(f"{kulcs} - {statisztika[kulcs]}", file=kimenet)
+
